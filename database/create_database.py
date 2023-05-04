cb30fb91a8bd34b9c18df400f859b6b7b4e1754b
Aiko <aikocute@icloud.com>
2023-05-04 22:48:43 +0700

    Update Create Database

--- database/create_database.py
+++ database/create_database.py
@@ -6,6 +6,30 @@ def check_mysql_installed():
         return True
     except subprocess.CalledProcessError:
         return False
+    
+def add_table():
+    import mysql.connector
+
+    # Connect to MySQL server
+    cnx = mysql.connector.connect(user='user', password='password', host='localhost', database='mysql')
+
+    # Tạo cơ sở dữ liệu
+    cursor = cnx.cursor()
+    create_database_query = f'CREATE DATABASE {name}'
+    cursor.execute(create_database_query)
+    cnx.database = name
+
+    # Tạo bảng plan
+    create_plan_table_query = 'CREATE TABLE `plan` (`id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `price` decimal(10,2) NOT NULL, `status` tinyint(1) NOT NULL DEFAULT \'0\', PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+    cursor.execute(create_plan_table_query)
+
+    # Tạo bảng user
+    create_user_table_query = 'CREATE TABLE `user` (`id` int(11) NOT NULL AUTO_INCREMENT, `udid_apple` varchar(50) NOT NULL, `type_of_device` varchar(50) NOT NULL, `telegram_id` varchar(50) DEFAULT NULL, `purchase_date` date DEFAULT NULL, `date_update` date DEFAULT NULL, `wait` date NOT NULL DEFAULT \'1970-01-01\', PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+    cursor.execute(create_user_table_query)
+
+    cursor.close()
+    cnx.close()
+
 
 def create_database(ip='localhost', port='3306', user=None, password=None, name=None):
     # Kiểm tra nếu tên người dùng và mật khẩu được cung cấp
@@ -47,3 +71,4 @@ name = input("Nhập tên cơ sở dữ liệu mới: ")
 
 # Tạo cơ sở dữ liệu
 create_database(ip, port, user, password, name)
+add_table()
