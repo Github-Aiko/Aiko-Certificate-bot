cfe15189ee35a5dd3659ca077ee28dc98428aa2a
Aiko <aikocute@icloud.com>
2023-05-04 19:01:00 +0700

    Update myinfo.py

--- Commands/myinfo.py
+++ Commands/myinfo.py
@@ -25,7 +25,9 @@ async def exec(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     msg = update.effective_message
     user_id = msg.from_user.id
     chat_type = msg.chat.type
-    if chat_type == 'private':
+    chat_id = msg.chat_id
+    
+    if chat_type == 'private' or chat_id == bot.config['bot']['group_id']:
         target_telegram_id = user_id
         if target_telegram_id:
             user_info = get_user_info_by_telegram_id(target_telegram_id)
@@ -34,19 +36,16 @@ async def exec(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                 wait_date_str = user_info[4].strftime('%Y-%m-%d')
                 wait_date_obj = datetime.strptime(wait_date_str, '%Y-%m-%d')
                 wait_days = (wait_date_obj.date() - datetime.now().date()).days
-                if wait_days <= 0:
-                    info_str = f"Thông tin của bạn:\n\n" \
-                            f"UDID Apple: {user_info[1]}\n" \
-                            f"Loại thiết bị: {user_info[2]}\n" \
-                            f"Trạng thái: Đang hoạt động!\n"
+                info_str = f"Thông tin của bạn:\n\n" 
+                if chat_type == 'private':
+                    info_str += f"UDID Apple: {user_info[1]}\n"
+                info_str += f"Loại thiết bị: {user_info[2]}\n"           
+                info_str += f"Ngày mua: {purchase_date}\n"
+                if wait_days > 0:
+                    info_str += f"Trạng thái: Đang Hoạt Động\n" 
                 else:
-                    info_str = f"Thông tin của bạn:\n\n" \
-                            f"UDID Apple: {user_info[1]}\n" \
-                            f"Loại thiết bị: {user_info[2]}\n" \
-                            f"Ngày mua: {purchase_date}\n" \
-                            f"Thời gian đợi: {wait_days} ngày\n" \
-                            f"Ngày đợi còn lại: {wait_date_obj.strftime('%d/%m/%Y')}"
-
+                    info_str += f"Thời gian đợi: {wait_days} ngày\n"       
+                    info_str += f"Ngày đợi còn lại: {wait_date_obj.strftime('%d/%m/%Y')}"
                 await msg.reply_markdown(info_str)
             else:
                 await msg.reply_markdown("Bạn chưa liên kết tài khoản Telegram với tài khoản của chúng tôi!")
